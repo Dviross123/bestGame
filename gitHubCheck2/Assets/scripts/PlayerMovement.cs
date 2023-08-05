10e538324f9051adb3e2a2a7b2872f5b6ff5b01f
idonthaveaname58 <idonaim8@gmail.com>
2023-08-05 18:55:18 +0300

    boucePad added
    if you have errors saying there are comflicted files delete the ones that are not it the correct folders

--- gitHubCheck2/Assets/scripts/PlayerMovement.cs
+++ gitHubCheck2/Assets/scripts/PlayerMovement.cs
@@ -220,7 +220,7 @@ public class PlayerMovement : MonoBehaviour
         else if (!isWallJumping && !JumpSliding)
         {
             stopSpeed = 1f;
-            rb.velocity = new Vector2(horizontal * speed + extraMomentum * extraMomentumDirection, rb.velocity.y);
+            rb.velocity = new Vector2(horizontal * speed + extraMomentum * extraMomentumDirection + BouncingSpeed * BouncingDirection, rb.velocity.y);
         }
     }
 
