e3a8943f6c7d84bafccad473be64e1327d10c2db
idonthaveaname58 <idonaim8@gmail.com>
2023-08-06 11:01:37 +0300

    fix damage on seeker
    also changed the robot to make it more responsive

--- gitHubCheck2/Assets/scripts/playerManager.cs
+++ gitHubCheck2/Assets/scripts/playerManager.cs
@@ -351,7 +351,10 @@ public class playerManager: MonoBehaviour
         player.GetComponent<playerManager>().health = oldHealth;
     }
 
-
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+    }
 
 
 }
