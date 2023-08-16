9371d1f2e8da7e65fbab499791097c9d4e7655ae
lahavior <128811783+lahavior@users.noreply.github.com>
2023-08-16 12:55:48 +0300

    level 2 and spikes knockback

--- gitHubCheck2/Assets/spike.cs
+++ gitHubCheck2/Assets/spike.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class spike : MonoBehaviour
 {
     public playerManager playerManager;
+    public PlayerMovement player;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool canDamage = true;
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && canDamage)
         {
             playerManager.takeDamage(1);
+            StartCoroutine(playerManager.playerHit(player.transform.localScale.x));
+            if(!player.isDashing) player.canDash = true;
+            player.Jumps = 0;
+            StartCoroutine(damageWait());
         }
+    }
+
+    private IEnumerator damageWait()
+    {
+        canDamage = false;
+        yield return new WaitForSeconds(0.5f);
+        canDamage = true;
     }
 }
